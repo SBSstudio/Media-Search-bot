dfa6b999fba108c95d2f71db57d47592bf1a5436
SBSstudio <86954238+SBSstudio@users.noreply.github.com>
2021-10-01 00:19:32 -0700

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -66,7 +66,7 @@ async def filter(client, message):
                     [InlineKeyboardButton(text=f"{filename}",callback_data=f"subinps#{file_id}")]
                     )
         else:
-            await client.send_sticker(chat_id=message.from_user.id, sticker='CAADBQADMwIAAtbcmFelnLaGAZhgBwI')
+            await client.send_sticker(chat_id=message.from_user.id, sticker='CAACAgIAAxkBAAJFJ2FWtmf_-6YHXPCViJ2pSjqIUMPzAAJPAAMkcWIaWMzn4qdTlUgeBA')
             return
 
         if not btn:
